8b1c81cc28dfd07281bef599600da370eee404b5
Eduardo J.S <103137675+Eduardo-J-S@users.noreply.github.com>
2023-12-06 23:16:50 -0300

    token e session

--- client/client.py
+++ client/client.py
@@ -3,33 +3,62 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-print("Aplicação Flask iniciada!")
-
 # URL da API do chatbot
-api_url = 'http://127.0.0.1:5000/chat'
+api_url = 'https://chatbot-sd-server.onrender.com'
 
 # Histórico do chat
 chat_history = []
+
+# Token de autenticação
+token = None
+
+@app.route('/login', methods=['GET'])
+def show_login():
+    return render_template('login.html')
 
 @app.route('/')
 def index():
     return render_template('index.html', chat_history=chat_history)
 
+# No client.py, altere a função login
+@app.route('/login', methods=['POST'])
+def login():
+    global token
+    username = request.form['username']
+    password = request.form['password']
+
+    data = {'username': username, 'password': password}
+    response = requests.post(f'{api_url}/login', json=data, timeout=30)
+
+    if response.status_code == 200:
+        token = response.json()['access_token']
+        return 'Login realizado com sucesso.'
+    else:
+        try:
+            # Tente fazer o parsing da resposta como JSON apenas se a requisição for bem-sucedida
+            response_data = response.json()
+            return f'Erro no login: {response.status_code} - {response_data}'
+        except:
+            # Se ocorrer um erro ao fazer o parsing, retorne uma mensagem genérica de erro
+            return f'Erro no login: {response.status_code} - Resposta inválida'
+
 @app.route('/send_message', methods=['POST'])
 def send_message():
-    user_input = request.form['user_input']
-    chatbot_response = get_chatbot_response(user_input)
-    chat_history.append({'user_input': user_input, 'chatbot_response': chatbot_response})
-    return render_template('index.html', chat_history=chat_history)
+    global token
+    if not token:
+        return 'Faça o login primeiro.'
 
-def get_chatbot_response(user_input):
+    user_input = request.form['user_input']
+    headers = {'Authorization': f'Bearer {token}'}
     data = {'user_input': user_input}
-    response = requests.post(api_url, json=data)
+    response = requests.post(f'{api_url}/chat', json=data, headers=headers)
+    
     if response.status_code == 200:
         response_data = response.json()
-        return response_data['response']
+        chat_history.append({'user_input': user_input, 'chatbot_response': response_data['response']})
+        return render_template('index.html', chat_history=chat_history)
     else:
         return f"Erro na solicitação: {response.status_code}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5001)
+    app.run(host='0.0.0.0', debug=False, port=5001)
--- server/server.py
+++ server/server.py
@@ -1,14 +1,21 @@
 import spacy
 import json
 import random
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_session import Session
 
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['JWT_SECRET_KEY'] = 'your-secret-key' 
+app.config['SESSION_PERMANENT'] = False
+jwt = JWTManager(app)
+Session(app)
 
 class SimpleChatBot:
     def __init__(self, intents):
         self.intents = intents
-
+    
     def get_response(self, user_input):
         doc = nlp(user_input)
         best_match = None
@@ -20,16 +27,11 @@ class SimpleChatBot:
             for tag in intent["tag"]:
                 for text in doc:
                     similarity = self.calculate_similarity(text, tag)
-                    print(text)
-                    print(similarity)
                     if similarity > 0.53 and similarity > best_similarity_tag:
                         best_similarity_tag = similarity
                         best_match_tag = intent
-                print('--------------------------------------------')
-        
         
         if best_match_tag:
-            print("Melhor correspondência de tag:", best_match_tag)
             pass
         else:
             return "Desculpe, eu não entendi. Pode reformular?"
@@ -48,7 +50,52 @@ class SimpleChatBot:
     def calculate_similarity(self, doc, pattern):
         pattern_tokens = nlp(pattern)
         return max(doc.similarity(pattern_tokens), pattern_tokens.similarity(doc))
-    
+
+
+class User:
+    def __init__(self, users):
+        self.users = users
+
+    def verify_credentials(self, username, password):
+        print(f"Verificando credenciais para usuário: {username}")
+
+        user = self.users.get(username)
+
+        if user and user["password"] == password:
+            print(f"Credenciais válidas para {username}")
+            return user
+        else:
+            print(f"Credenciais inválidas para {username}")
+            return None
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        print("Entrou na rota de login")
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        print(f"Recebido username: {username}, password: {password}")
+
+        # Verifica as credenciais no banco de dados
+        user = userStart.verify_credentials(username, password)
+
+        print("Verificou credenciais")
+
+        if user:
+            # Gera um token JWT com o identificador do usuário
+            access_token = create_access_token(identity=username)
+            print(f"Login bem-sucedido para {username}. Token: {access_token}")
+            return jsonify(access_token=access_token)
+        else:
+            print(f"Credenciais inválidas para {username}")
+            return jsonify({'error': 'Credenciais inválidas'}), 401
+    except Exception as e:
+        print(f"Erro durante o login: {str(e)}")
+        return jsonify({'error': 'Erro interno durante o login'}), 500
+
 
 # Carregando o modelo do spaCy
 nlp = spacy.load("pt_core_news_md")
@@ -57,15 +104,27 @@ nlp = spacy.load("pt_core_news_md")
 with open("intents.json", encoding='utf-8')  as f:
     intents = json.load(f)
 
-# Inicializando o chatbot
-chatbot = SimpleChatBot(intents)
+# Carregando os usuários do arquivo JSON
+with open("users.json", encoding='utf-8') as f:
+    users = json.load(f)
+
+# Inicializando o user
+userStart = User(users)
 
 # Endpoint para receber mensagens do usuário e retornar respostas do chatbot
 @app.route('/chat', methods=['POST'])
+@jwt_required()  # Protege este endpoint com autenticação JWT
 def chat():
-    data = request.get_json()
-    user_input = data['user_input']
-    response = chatbot.get_response(user_input)
+    current_user = get_jwt_identity()
+    user_input = request.get_json()['user_input']
+    # Use o nome de usuário como parte da chave da sessão para manter instâncias separadas
+    chatbot_instance = f"chatbot_{current_user}"
+    # Verifica se a instância do chatbot já existe na sessão
+    if chatbot_instance not in session:
+        # Se não existir, cria uma nova instância do chatbot
+        session[chatbot_instance] = SimpleChatBot(intents)
+    # Obtém a resposta usando a instância do chatbot específica do usuário
+    response = session[chatbot_instance].get_response(user_input)
     return jsonify({'response': response})
 
 # Executando o aplicativo Flask localmente
